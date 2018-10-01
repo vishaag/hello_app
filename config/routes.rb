b2b30dce5230f8d990c29292a4194adabd69b57f
vishaag <vishaag@gmail.com>
2018-10-01 13:29:46 +0530

    Added layouts

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
+  get 'static_pages/about'
+
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'static_pages#home'
   
 end
